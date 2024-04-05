2fd791da19cb7540ebef8df8500670468b979a4a
hasmukhbhadani9983 <hasmukhbhai.bhadani@bank-abc.com>
2024-04-05 16:55:27 +0530

    [100-New activity created]

---
+++ app/src/main/java/com/hb/myapplication/ActivityB.kt
@@ -0,0 +1,18 @@
+package com.hb.myapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ActivityB : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+    }
+    fun addition() {
+
+    }
+}
